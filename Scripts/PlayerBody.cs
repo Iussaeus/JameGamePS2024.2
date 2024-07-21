a975134a3d65aa98b93a0a70ab86053479211ff4
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2024-07-21 14:10:25 +0300

    Added unique scene name and a basic dash mechanic

--- Scripts/PlayerBody.cs
+++ Scripts/PlayerBody.cs
@@ -1,37 +1,44 @@
 using Godot;
 using Godot.Collections;
-using Test.Scripts;
+
+namespace Test.Scripts;
 
 public partial class PlayerBody : CharacterBody3D
 {
-    [Export] public const float Speed = 5.0f;
+    // Ray-casting shenanigans
+    private const float RayLeN = 1000f;
+    private Camera3D _camera3D;
 
-    [Export] public const float JumpVelocity = 4.5f;
-
-    // Raycasting shenanigans
-    public const float RayLeN = 1000f;
-    public Camera3D Camera3D;
+    private bool _canDash = true;
+    [Export] private int _dashCooldown = 2;
+    private Timer _dashTimer;
 
     // Get the gravity from the project settings to be synced with RigidBody nodes.
-    public float Gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    private float _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+    [Export] public float JumpVelocity = 4.5f;
+    [Export] public float Speed = 5.0f;
 
     public override void _Ready()
     {
-        Camera3D = GetParent<SubViewport>().GetNodeOrNull<Camera3D>("Camera3D");
-        this.AssertMeDaddy(Camera3D != null, "HAHAHAHAHAHA");
+        _camera3D = GetNode<Camera3D>("%Camera3D");
+        _dashTimer = new Timer();
+        AddChild(_dashTimer);
+        _dashTimer.OneShot = true;
+        _dashTimer.Timeout += () => _canDash = true;
     }
 
     public override void _Process(double delta)
     {
         var mousePos = GetViewport().GetMousePosition();
-        var from = Camera3D.ProjectRayOrigin(mousePos);
-        var to = from + Camera3D.ProjectRayNormal(mousePos) * RayLeN;
+        var from = _camera3D.ProjectRayOrigin(mousePos);
+        var to = from + _camera3D.ProjectRayNormal(mousePos) * RayLeN;
         var directSpaceState = GetWorld3D().DirectSpaceState;
         var query = PhysicsRayQueryParameters3D.Create(from, to);
         query.Exclude = new Array<Rid> { GetRid() };
         var intersection = directSpaceState.IntersectRay(query);
 
-        if (Input.IsActionPressed("right_click") && intersection.TryGetValue("position", out var value))
+        if (Input.GetLastMouseVelocity() != Vector2.Zero && intersection.TryGetValue("position", out var value))
             LookAt((Vector3)value);
 
         GlobalRotation = new Vector3(0f, GlobalRotation.Y, 0f);
@@ -41,18 +48,12 @@ public partial class PlayerBody : CharacterBody3D
     {
         var velocity = Velocity;
 
-        // Add the gravity.
         if (!IsOnFloor())
-            velocity.Y -= Gravity * (float)delta;
+            velocity.Y -= _gravity * (float)delta;
 
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-            velocity.Y = JumpVelocity;
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
         var inputDir = Input.GetVector("left", "right", "forward", "backward");
         var direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+
         if (direction != Vector3.Zero)
         {
             velocity.X = direction.X * Speed;
@@ -63,6 +64,15 @@ public partial class PlayerBody : CharacterBody3D
             velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
             velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
         }
+
+        if (Input.IsActionJustPressed("space") && _canDash)
+        {
+            _canDash = false;
+            _dashTimer.Start(_dashCooldown);
+            velocity = direction * JumpVelocity;
+        }
+
+        GD.Print(_dashTimer.TimeLeft);
 
         Velocity = velocity;
         MoveAndSlide();
