e91ccaf68b4f9d7c655428f6e8d3eda1174ddc10
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2024-08-10 23:46:21 +0300

    improved the character controller and added few extension methods

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -6,17 +6,22 @@ namespace Test.Scripts.Player;
 
 public partial class PlayerController : CharacterBody3D
 {
+
+    [Export] public float DashCooldown = 2;
+    [Export] public float DashVelocity = 200;
+    [Export] public float DashAcceleration = 4;
+    [Export] public float Speed = 5;
+    [Export] public float HorizontalAcceleration = 10;
+
+    public Vector3 HorizontalVelocity = new();
+    public bool CanDash = true;
     private readonly Timer _dashTimer = new();
 
-    private readonly float _rayLen = 1000;
+    public float Gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
     private Camera3D _camera3D;
     private Gun _gun;
     private Marker3D _marker3D;
-    public bool CanDash = true;
-    [Export] public float DashCooldown = 2;
-    [Export] public float DashVelocity = 200;
-    public float Gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
-    [Export] public float Speed = 5;
+    private readonly float _rayLen = 1000;
 
     public override void _Ready()
     {
@@ -24,17 +29,17 @@ public partial class PlayerController : CharacterBody3D
 
         _marker3D = GetNode<Marker3D>("Marker3D");
         _gun = GetNode<Gun>("Gun");
-		AwaitCamera();
+        AwaitCamera();
         this.Assert(_camera3D != null, "Player Don't got no camera");
 
         _dashTimer.OneShot = true;
-        _dashTimer.Timeout += () => CanDash = false;
+        _dashTimer.Timeout += () => CanDash = true;
         AddChild(_dashTimer);
     }
 
     public async void AwaitCamera()
     {
-		await ToSignal(Globals.Instance, Globals.SignalName.CameraSpawned);
+        await ToSignal(Globals.Instance, Globals.SignalName.CameraSpawned);
         _camera3D = Globals.Camera;
     }
 
@@ -59,39 +64,70 @@ public partial class PlayerController : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-        var inputDir = Input.GetVector("left", "right", "forward", "backward");
-
-        Move(inputDir, (float)delta);
+        Move();
     }
 
-    public void Move(Vector2 inputDir, float delta)
+    public void Move()
     {
         var newVelocity = Velocity;
+		var delta = (float)GetPhysicsProcessDeltaTime();
 
         if (!IsOnFloor()) newVelocity.Y -= Gravity * delta;
 
-        var direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+        HorizontalVelocity = HorizontalVelocity.Lerp(GetDirection() * Speed,
+                HorizontalAcceleration * delta);
+        newVelocity.Z = HorizontalVelocity.Z;
+        newVelocity.X = HorizontalVelocity.X;
 
-        if (direction != Vector3.Zero)
-        {
-            newVelocity.X = direction.X * Speed;
-            newVelocity.Z = direction.Z * Speed;
-        }
-        else
-        {
-            newVelocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            newVelocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-        }
-
-        if (Input.IsActionJustPressed("space") && CanDash)
-        {
-            CanDash = false;
-            _dashTimer.Start();
-            newVelocity = direction * DashVelocity;
-        }
+        if (Input.IsActionJustPressed("space") && CanDash) Dash();
 
         Velocity = newVelocity;
 
         MoveAndSlide();
     }
+
+	public void Dash()
+	{
+		var newVelocity = Velocity;
+
+		CanDash = false;
+		_dashTimer.Start();
+		HorizontalVelocity = HorizontalVelocity.Lerp(GetDirection() * DashVelocity,
+				DashAcceleration * (float)GetPhysicsProcessDeltaTime());
+		newVelocity.Z = HorizontalVelocity.Z;
+		newVelocity.X = HorizontalVelocity.X;
+
+		Velocity = newVelocity;
+
+		MoveAndSlide();
+	}
+
+	public Vector3 GetDirection() 
+	{
+        var inputDir = Input.GetVector("left", "right", "forward", "backward");
+        var direction = new Vector3(0, 0, 0);
+
+        if (inputDir.X > 0)
+        { 
+			direction.X += -_camera3D.GlobalBasis.Y.X; 
+			direction.Z += -_camera3D.GlobalBasis.Y.Y; 
+		}
+        if (inputDir.X < 0) 
+		{
+			direction.X += _camera3D.GlobalBasis.Y.X;
+			direction.Z += _camera3D.GlobalBasis.Y.Y;
+		}
+        if (inputDir.Y > 0) 
+		{
+			direction.X += _camera3D.GlobalBasis.Z.X;
+			direction.Z += _camera3D.GlobalBasis.Z.Y;
+		}
+        if (inputDir.Y < 0) 
+		{
+			direction.X += -_camera3D.GlobalBasis.Z.X;
+			direction.Z += -_camera3D.GlobalBasis.Z.Y;
+		}
+
+		return direction.Normalized();
+	}
 }
