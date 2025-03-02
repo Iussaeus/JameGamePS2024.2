534581fe1efb6de7e10d569417ee62cb211eaec7
Iussaeus <118941666+Iussaeus@users.noreply.github.com>
2025-03-02 22:04:53 +0000

    commented debug print statements

--- Entities/Globals/Globals.cs
+++ Entities/Globals/Globals.cs
@@ -21,45 +21,46 @@ public partial class Globals : Node {
     public override void _EnterTree() => Instance = this;
 
     public async Task<T> AwaitSignalSingle<T>(StringName signal) where T : Node {
-        GD.Print($"Waiting for {signal}");
+        // GD.Print($"Waiting for {signal}");
 
         T result = default;
         var s = await ToSignal(SignalBus.Instance, signal);
-        GD.Print($"Done waiting for {signal}");
-        s.Print();
+        // GD.Print($"Done waiting for {signal}");
+        // s.Print();
         if (s.Length == 1) {
             result = (T)s[0];
         }
 
-        GD.Print($"result of{signal}: ", result);
+        // GD.Print($"result of{signal}: ", result);
 
         return result;
     }
 
     public async Task<T[]> AwaitSignalArray<T>(StringName signal) where T : Node {
-        GD.Print($"Waiting for {signal}");
+        // GD.Print($"Waiting for {signal}");
 
         var s = await ToSignal(SignalBus.Instance, signal);
 
-        GD.Print($"Done waiting for {signal}");
-        s.Print();
+        // GD.Print($"Done waiting for {signal}");
+        // s.Print();
         T[] result = new T[s.Length];
 
         for (int i = 0; i < result.Length; i++) {
             result[i] = (T)s[i];
         }
 
-        GD.Print($"result of{signal}: ", result);
-        result.Print();
+        // GD.Print($"result of{signal}: ", result);
+        // result.Print();
 
         return result;
     }
 
     public async Task<Variant[]> AwaitSignal(StringName signal) {
-        GD.Print($"Waiting for {signal}");
+        // GD.Print($"Waiting for {signal}");
         var s = await ToSignal(SignalBus.Instance, signal);
-        GD.Print($"Done waiting for {signal}");
-        s.Print();
+
+        // GD.Print($"Done waiting for {signal}");
+        // s.Print();
         return s;
     }
 }
